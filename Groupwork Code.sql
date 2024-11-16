ba33122599dc03e85fa6d9a6bc1b97c040ae74cf
Baraka Chemengich <167431415+BarakaChemengich@users.noreply.github.com>
2024-11-16 10:28:48 +0300

    Update Groupwork Code.sql

--- Groupwork Code.sql
+++ Groupwork Code.sql
@@ -4,7 +4,7 @@ CREATE TABLE School (
     Location VARCHAR(100),
     ContactInfo VARCHAR(50),
     PrincipalName VARCHAR(50);
-
+ 
 INSERT INTO School (SchoolID, Name, Location, ContactInfo, PrincipalName) VALUES
 (1, 'Greenfield Academy', 'Nairobi', '+254700123456', 'Jane Kimani'),
 (2, 'Lakeside High', 'Kisumu', '+254700789012', 'John Wafula'),
